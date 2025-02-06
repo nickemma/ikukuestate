04992020f74f107deeb87d5c753ab5808f83fc51
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2025-02-06 10:01:29 -0800

    Feat: done another fix

--- server/index.ts
+++ server/index.ts
@@ -24,6 +24,8 @@ app.use(
   cors({
     origin: ["http://localhost:5173", "https://ikukuestate.vercel.app"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
   })
 );
 // Handle preflight requests
